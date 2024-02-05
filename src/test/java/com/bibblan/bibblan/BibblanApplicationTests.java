b73f883dedc5e8287b3dcec1346d83a5d2d01621
Viktor Nyberg <viktor.nyberg00@gmail.com>
2024-02-05 17:26:49 +0100

    first commit yay

---
+++ src/test/java/com/bibblan/bibblan/BibblanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibblan.bibblan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibblanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
