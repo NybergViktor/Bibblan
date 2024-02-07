6a4da1f1885e4c19339a6c9c400f7df97ca0e0c3
Viktor Nyberg <viktor.nyberg00@gmail.com>
2024-02-07 13:04:55 +0100

    users and a bit of borrowedBooks

--- src/main/java/com/bibblan/bibblan/models/Users.java
+++ src/main/java/com/bibblan/bibblan/models/Users.java
@@ -1,4 +1,44 @@
 package com.bibblan.bibblan.models;
 
+import org.springframework.data.annotation.Id;
+
 public class Users {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+
+    private int age;
+
+    public Users() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
--- src/main/java/com/bibblan/bibblan/services/UsersService.java
+++ src/main/java/com/bibblan/bibblan/services/UsersService.java
@@ -1,11 +1,41 @@
 package com.bibblan.bibblan.services;
 
+import com.bibblan.bibblan.models.Users;
 import com.bibblan.bibblan.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UsersService {
     @Autowired
     UsersRepository usersRepository;
+
+    // add a new user
+    public Users createUsers(Users users) {
+        return usersRepository.save(users);
+    }
+
+    // get a list with all users
+    public List<Users> getAllUsers() {
+        return usersRepository.findAll();
+    }
+
+    // update a users information
+    public Users updateUsers(Users users) {
+        return usersRepository.save(users);
+    }
+
+    // get a specifik user using id
+    public Users usersById(String id) {
+        return usersRepository.findById(id).get();
+    }
+
+    // delete user
+    public String deleteUsers(String id) {
+        usersRepository.deleteById(id);
+        return "User with id: " + id + " has been deleted";
+    }
+
 }
